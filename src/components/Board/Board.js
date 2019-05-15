39fbd45a60e101a69838be6959b3dc0f2de855f5
Patryk Stanek <p.stanek3@gmail.com>
2019-05-15 18:29:34 +0200

    base components loaded

---
+++ src/components/Board/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Board.css';
+
+class Board extends React.Component {
+
+    render() {
+        return (
+            <p className={style.Main}>Board loaded!</p>
+        )
+    }
+}
+
+export default Board;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,12 +3,19 @@ import {hot} from 'react-hot-loader';
 
 import style from './App.css';
 
+import Board from '../components/Board/Board.js';
+import Controls from '../components/Controls/Controls.js';
+import Numbers from '../components/Numbers/Numbers.js';
+
 class App extends React.Component {
     
     render() {
         return (
             <div className={style.Main}>
                 <h1>Hello world!</h1>
+                <Board />
+                <Numbers />
+                <Controls />
             </div>
         )
     }
