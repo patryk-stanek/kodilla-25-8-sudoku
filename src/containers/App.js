07b7b96e30df6105a6151eb289b0beaf5234928a
Patryk Stanek <p.stanek3@gmail.com>
2019-05-15 18:21:32 +0200

    included style and component construction

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 
+import style from './App.css';
+
 class App extends React.Component {
     
     render() {
         return (
-            <div>
+            <div className={style.Main}>
                 <h1>Hello world!</h1>
             </div>
         )
