576e960ae5757948c9fa8ad0bd40cb8cdeea92c4
Patryk Stanek <p.stanek3@gmail.com>
2019-05-19 00:39:33 +0200

    added newGame/reset/solve/check functions

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,30 +13,56 @@ class App extends React.Component {
         super(props);
         this.state = {
             initialBoard: [],
-            board: []
+            board: [],
+            game: ''
         }
-        this.sudoku = this.sudoku.bind(this);
+        this.newGame = this.newGame.bind(this);
+        this.solve = this.solve.bind(this);
+        this.reset = this.reset.bind(this);
+        this.check = this.check.bind(this);
     }
 
     componentWillMount() {
-        const x = sudoku.generate('easy');
-        const y = x.split('');
-        this.setState({initialBoard: y, board: y});
+        const init = sudoku.generate('easy');
+        const array = init.split('');
+        this.setState({initialBoard: array, board: array});
     }
 
-    sudoku(){
+    newGame() {
         this.setState({board: []});
-        const x = sudoku.generate('easy');
-        const y = x.split('');
-        this.setState({board: y});
+        const init = sudoku.generate('easy');
+        const array = init.split('');
+        this.setState({board: array, initialBoard: array});
+    }
+
+    reset() {
+        this.setState({board: this.state.initialBoard});
+    }
+
+    solve() {
+        const init = sudoku.solve(this.state.board);
+        const array = init.split('');
+        this.setState({board: array})
+    }
+
+    check() {
+        let init = sudoku.solve(this.state.initialBoard);
+        const compare = this.state.board;
+        const convert = compare.join('');
+
+        init == convert ? this.setState({game: 'Win'}) : this.setState({game: 'Lose'});
     }
     
     render() {
         return (
             <div className={style.Main}>
-                <button onClick={this.sudoku}>sudoku</button>
+                <button onClick={this.newGame}>New Game</button>
                 <h1>Sudoku</h1>
                 <Board data={this.state.board}/>
+                <button onClick={this.solve}>Solve</button>
+                <button onClick={this.reset}>Reset</button>
+                <button onClick={this.check}>Check</button>
+                <h2>{this.state.game}</h2>
                 <Numbers />
                 <Controls />
             </div>
